709668dd7ce46c302cf70155b9c5b79b76b8b786
hey-rum-ba <siddhartcool2000@gmail.com>
2021-07-15 22:19:31 +0530

    initial commit

---
+++ BST_Insertion.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+    }
+
+/*
+Node is defined as 
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    Node * insert(Node * root, int data) {
+        Node * newNode = new Node(data);
+    newNode->left = NULL;
+    newNode->right = NULL;
+    if(root == NULL){
+       root = newNode;
+    }
+    else if(root->data < data ){
+        root->right = insert(root->right , data);
+    } 
+    else root->left = insert(root->left , data);
+    return root;
+
+
+    }
+
+};
