9b4271b9baa282e1cee303b8767fa9853126cc98
hey-rum-ba <siddhartcool2000@gmail.com>
2021-07-23 20:42:47 +0530

    initial commit

---
+++ Day16Exception.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    string S;
+    getline(cin, S);
+    try
+    {
+        cout<<stoi(S)<<endl;
+    }
+    catch(exception e)
+    {
+        cout<<"Bad String";
+    }
+    return 0;
+}
