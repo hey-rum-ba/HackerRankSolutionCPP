72c52206c51d11ebca37b6617fe33339304e4fbd
hey-rum-ba <siddhartcool2000@gmail.com>
2021-07-29 14:40:38 +0530

    inital commit

---
+++ Day28RegEXPatternsAndIntroToDatabases.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool end_with(string& s) {
+    string t = "@gmail.com";
+    if (s.size() < t.size()) return false;
+    for (int i = s.size() - 1, j = t.size() - 1; i >= 0 && j >= 0; --i, --j) {
+        if (s[i] != t[j]) return false;
+    }
+    return true;
+}
+int main(){
+    int N;
+    cin >> N;
+    vector<string> ans;
+    for(int a0 = 0; a0 < N; a0++){
+        string firstName;
+        string emailID;
+        cin >> firstName >> emailID;
+        if (end_with(emailID))
+            ans.push_back(firstName);
+    }
+    sort(ans.begin(), ans.end());
+    for (int i = 0; i < ans.size(); ++i)
+        cout << ans[i] << endl;
+    return 0;
+}
