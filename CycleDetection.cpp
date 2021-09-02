9e987176563f7421d7c41e872f7f1f834d775d95
hey-rum-ba <siddhartcool2000@gmail.com>
2021-09-02 17:40:00 +0530

    initial commit

---
+++ CycleDetection.cpp
@@ -0,0 +1,24 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+    if (head == NULL) return false;
+    
+    Node *slow = head;
+    Node *fast = head->next;
+    while (slow != fast) {
+        if (fast == NULL || fast->next == NULL) return false;
+        
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return true;
+}
