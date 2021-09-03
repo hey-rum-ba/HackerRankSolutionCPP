4577ce852f48da950473749d6ae308f804a2953c
hey-rum-ba <siddhartcool2000@gmail.com>
2021-09-03 21:13:45 +0530

    initial commit

---
+++ text2.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<vector<int>>
+#define vpi vector<pair<int,int>
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define rrep(i,a,b) for (int i = a; i > b; i--)
+#define repit(i,a,b) for (int i = a; i >= b; i--)
+#define iter(it,c) for (__typeof(c.begin()) it = c.begin(); it != c.end(); it++)
+#define CIN(n) int n;cin>>n;while(n--)
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); 
+
+#define ADD(x) x + x
+#define SUB(x) x - x
+int main()
+{ int y = ADD(3) / SUB(3); 
+printf("%d", ADD(y));
+}
