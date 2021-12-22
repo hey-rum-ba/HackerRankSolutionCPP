9c4cdf75561f59e98830ca9a207e1e69017ee4ab
hey-rum-ba <siddhartcool2000@gmail.com>
2021-12-22 20:32:52 +0530

    initial commit

--- TwoCharacters.cpp
+++ TwoCharacters.cpp
@@ -6,79 +6,127 @@ string ltrim(const string &);
 string rtrim(const string &);
 
 /*
- * Complete the 'alternate' function below.
+ * Complete the 'gemstones' function below.
  *
  * The function is expected to return an INTEGER.
- * The function accepts STRING s as parameter.
+ * The function accepts STRING_ARRAY arr as parameter.
  */
 
-int alternate(string s) {
-    if (s.empty())
-    return s.size();
+int gemstones(vector<string> arr) {
+     set<string> res;
 
-  string unique;
-  for (auto c = s.cbegin(); c < s.cend(); ++c)
-    if (unique.find(*c) == string::npos)
-      unique.push_back(*c);
-
-  unsigned crossIndex[26];
-  for (auto i = 0; i < 26; ++i)
-    crossIndex[i];
-  for (auto i = 0; i < unique.size(); ++i)
-    crossIndex[('z' - unique[i])] = i;
-
-  const string::size_type ts = unique.size();
-  char  letter[ts][ts];
-  int   count[ts][ts];
-  for (auto i = 0; i < ts; ++i)
-    for (auto j = 0; j < ts; ++j)
-      count[i][j] = 0;
-      
-  for (auto c = s.cbegin(); c < s.cend(); ++c)
-  {
-    const unsigned index = crossIndex['z' - *c];
-
-    for (auto i = 0; i < ts; ++i)
-    {
-      if (count[index][i] != -1 && letter[index][i] != *c)
-      {
-        letter[index][i] = *c;
-        ++count[index][i];
-      }
-      else
-        count[index][i] = -1;
-
-      if (count[i][index] != -1 && letter[i][index] != *c)
-      {
-        letter[i][index] = *c;
-        ++count[i][index];
-      }
-      else
-        count[i][index] = -1;
+    for (int i =0;i<arr.size();i++) {
+        res.insert(arr[i]);
     }
-  }
-
-  int result = 0;
-  for (auto i = 0; i < ts; ++i)
-    for (auto j = 0; j < ts; ++j)
-      result = max(result, count[i][j]);
-
-  return (result > 1 ? result : 0);
+    for (int i=1; i<arr.size(); i++) {
+        for (auto c : res) {
+            if (arr[i].find(c) == string::npos) {
+                res.erase(c);
+            }
+        }
+        if (res.size() == 0) break;
+    }
+ return res.size();
 }
 
 int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
 
-    string l_temp;
-    getline(cin, l_temp);
+    string n_temp;
+    getline(cin, n_temp);
 
-    int l = stoi(ltrim(rtrim(l_temp)));
+    int n = stoi(ltrim(rtrim(n_temp)));
 
-    string s;
-    getline(cin, s);
+    vector<string> arr(n);
 
-    int result = alternate(s);
+    for (int i = 0; i < n; i++) {
+        string arr_item;
+        getline(cin, arr_item);
+
+        arr[i] = arr_item;
+    }
+
+    int result = gemstones(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gemstones' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING_ARRAY arr as parameter.
+ */
+
+int gemstones(vector<string> arr) {
+     set<string> res;
+
+    for (int i =0;i<arr.size();i++) {
+        res.insert(arr[i]);
+    }
+    for (int i=1; i<arr.size(); i++) {
+        for (auto c : res) {
+            if (arr[i].find(c) == string::npos) {
+                res.erase(c);
+            }
+        }
+        if (res.size() == 0) break;
+    }
+ return res.size();
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        string arr_item;
+        getline(cin, arr_item);
+
+        arr[i] = arr_item;
+    }
+
+    int result = gemstones(arr);
 
     fout << result << "\n";
 
